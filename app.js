35d0006e5f0887996ca176838c7103e514d66bba
vinit <vinitjodhpur2@gmail.com>
2024-02-22 22:23:32 +0530

    msg

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ app.get("/",(req,res)=>{
     res.send("working");
 })
 
-app.get("/testlisting",async (req,res)=>{
-    let samplelisting = new Listing({
-        title:"my new villa",
-        description:"by the beach",
-        price:1200,
-        location:"jodhpur",
-        country:"india,"
-    })
-    await samplelisting.save();
-    console.log("data saved succesfuuly");
-    res.send("succesfully testing ")
-})
+// app.get("/testlisting",async (req,res)=>{
+//     let samplelisting = new Listing({
+//         title:"my new villa",
+//         description:"by the beach",
+//         price:1200,
+//         location:"jodhpur",
+//         country:"india,"
+//     })
+//     await samplelisting.save();
+//     console.log("data saved succesfuuly");
+//     res.send("succesfully testing ")
+// })
 app.listen(8080,()=>{
     console.log("listneing to the port")
 })
