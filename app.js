b352faf48b9c86db167384536f2bc94920b2e83f
vinit <vinitjodhpur2@gmail.com>
2024-02-24 08:01:53 +0530

    project

--- app.js
+++ app.js
@@ -2,31 +2,111 @@ const express = require("express");
 const app = express();
 const Listing = require("./models/listing.js");
 const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override")
+const { count } = require("console");
+const ejsMate = require("ejs-mate");
 
-main().then(()=>{
+
+app.use(express.urlencoded({extended:true}));
+app.set("views engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")))
+
+
+main().then(() => {
     console.log("connected to the DB");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
-app.get("/",(req,res)=>{
+
+app.get("/", (req, res) => {
     res.send("working");
 })
 
-// app.get("/testlisting",async (req,res)=>{
-//     let samplelisting = new Listing({
-//         title:"my new villa",
-//         description:"by the beach",
-//         price:1200,
-//         location:"jodhpur",
-//         country:"india,"
-//     })
-//     await samplelisting.save();
-//     console.log("data saved succesfuuly");
-//     res.send("succesfully testing ")
-// })
-app.listen(8080,()=>{
-    console.log("listneing to the port")
+//INDEX ROUTE
+app.get("/listings", async (req, res) => {
+    try {
+        const alllistings = await Listing.find({});
+        // console.log(alllistings);
+        // res.send(listings); // Sending the listings retrieved from the database
+        res.render("listings/index.ejs",{alllistings});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching data"); // Sending an error response if there's an issue
+    }
+})
+ 
+//NEW ROUTE
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/newform.ejs");
+})
+
+// SHOW ROUTE 
+app.get("/listings/:id",async (req,res)=> {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    // res.send("showing");
+    res.render("listings/show.ejs",{listing})
+    // console.log(listing);
+});
+
+app.post("/listings",async(req,res)=>{
+    // let {title,description,image,price,location,country} = req.body;
+    // let newListing = new Listing({
+    //     title:title,
+    //     description:description,
+    //     image:image,
+    //     price:price,
+    //     location:location,
+    //     country:country,
+    // });
+
+    // this was the first way to store the data and i am simply using the second tay that is more ssimpler
+
+    //second way is that take all the data rom the req.body and save that in variable and pass with the variable to model 
+   
+    // let listing = req.body;
+    // newlisting = await new Listing(listing)
+    // newListing.save()
+    
+    let newListing = new Listing(req.body.listing);//req.body se humne jb data liya and console p kra to listings key m data aaraha hai to humne kiya ki listings ko .opertor se uss key ki value ko acccess kiay
+
+    await newListing.save();
+    console.log(newListing);
+    res.redirect("/listings")
+})
+
+
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    // console.log(listing);
+    res.render("listings/edit.ejs",{listing});
+
+})
+
+//update route
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    console.log(req.body);
+    await Listing.findByIdAndUpdate(id,{...req.body});
+    res.redirect("/listings");
+})
+//delete route
+app.delete("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    let deletedlistings = await Listing.findByIdAndDelete(id);
+    console.log(deletedlistings);
+    res.redirect("/listings");
+})
+app.listen(8080, () => {
+    console.log("listening on port 8080");
 })
--- init/index.js
+++ init/index.js
@@ -1,20 +1,31 @@
-const initData = require("./data.js")
+const initData = require("./data.js");
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
-main().then(()=>{
-    console.log("connected to the DB");
-}).catch((err)=>{
+
+main()
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-const initDB = async()=>{
-    //agar koi data phle se ho to usko delete krdo
+const initDB = async () => {
+  try {
     await Listing.deleteMany({});
+    // const listings = initData.data.map((item) => ({
+    //   ...item,
+    //   image: item.image.url, // Assuming the image URL is stored within the 'url' property of the 'image' object
+    // }));
     await Listing.insertMany(initData.data);
-     //data:samplelisting jo hum export kr rhe hai and idhr hum initData m hum require kr rhe hai and uski hum key(data ) se initialize krna chahte hai humare collection ko.
-    console.log("saved");
-}
+    console.log("Data saved successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+};
+
 initDB();
